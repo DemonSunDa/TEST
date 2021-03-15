96451df535489d7a47624e064c2f0e973dacac52
DemonSunDa <demonsd98@outlook.com>
2021-03-15 19:21:24 +0800

    HELLO GITHUB

--- TEST.c
+++ TEST.c
@@ -2,10 +2,7 @@
 
 int main(void)
 {
-    // int L;
-    unsigned char L;
-    L = 0b0011;
-    L <<= 4;
-    // L = L << 2;
-    printf("%X\n", L);
+    printf("HELLO GITHUB\n");
+
+    return 0;
 }
