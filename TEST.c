b6354463c6ab4bd8016918ce99e476b4b14fcd85
DemonSunDa <demonsd98@outlook.com>
2021-03-15 19:56:32 +0800

    combine

--- TEST.c
+++ TEST.c
@@ -2,7 +2,13 @@
 
 int main(void)
 {
-    printf("HELLO GITHUB\n");
+    unsigned char bin;
+
+    bin = 0b00000010;
+    bin <<= 2;
+
+    printf("%X\n", bin);
+    printf("HELLO GITHUB\n\n");
 
     return 0;
 }
